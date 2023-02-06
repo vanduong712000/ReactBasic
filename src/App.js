a5c3754a9e08fb439a57d56cbf344bacc10e5f9d
Duong CV <duongnv01397.dnv01397@gmail.com>
2023-02-06 11:21:02 +0700

    react basic

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+import React from 'react';
+
+  const App = () => {
+
+      return (
+        <div className="app-container">
+        <Header />
+      </div>
+      );
+    }
+
+
+
+
+
+     // //<div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <div>Count = {count}</div>
+    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+    //   </header>
+    // //</div>
+ 
+   
+  export default App;
+
+
+    
+   
+
+
+
